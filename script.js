3aa317f548f616354eb8bb18b06c82d366a0137c
Caspian Almerud <almerudcaspian@gmail.com>
2025-03-18 11:11:56 +0100

    Second commit from mob session

--- script.js
+++ script.js
@@ -10,22 +10,21 @@ const elements = {
 
 createWeatherCard = (weather) => {
   const weatherCard = `
-    <div class="weather-content">
-      <div style="position: relative;">
-                    {/* Shows temperature, sunrise and sunset and weather description,  */}
-          <div class="today-info"> 
-          <img id="weather-icon" src="" alt="">
-          <div class="weather-description">
-          <div class="weather-forecast">
-              <h4>City</h4>
-              <ul>
-                Day 1 weather
-                Day 2 weather
-                Day 3 weather
-                Day 4 weather
-              </ul>
-          </div>
-      </div>
+    <div id="weather-content" class=${weather-condition}>
+        {/* Shows temperature, sunrise and sunset and weather description,  */}
+        <div id="today-info"> 
+        <img id="weather-icon" src="" alt="">
+        <div id="weather-description">
+        <div id="weather-forecast">
+            <h4>City</h4>
+            <ul>
+              Day 1 weather
+              Day 2 weather
+              Day 3 weather
+              Day 4 weather
+            </ul>
+        </div>
+    </div>
     </div>
     `;
 };
