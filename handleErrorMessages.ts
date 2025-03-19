4fd679ea6fb8c2897eba3eaa7d4c7b14439b0e17
Linda Schönfeldt <95916410+LindaSchonfeldt@users.noreply.github.com>
2025-03-19 09:24:10 +0100

    Added function to handle error messages

---
+++ handleErrorMessages.ts
@@ -0,0 +1,22 @@
+// Function to handle error messages
+const handleErrorMessages = (error: string) => {
+  console.error("Error detected:", error)
+
+  let message = "An unexpected error occurred. Please try again."
+  let messageType = "warning"
+  if (!supportsGeolocation) {
+    message = "Geolocation is not supported by your browser."
+    messageType = "error"
+  } else {
+    console.error("Error fetching data:", error)
+    message = "Failed to load recipes. Please try again later."
+  }
+
+  // Update the message in the HTML
+  const errorContainer = document.getElementById("error-container")
+  if (errorContainer) {
+    errorContainer.innerHTML = `<h3>${message}</h3>`
+    errorContainer.className = messageType
+    errorContainer.style.display = "block"
+  }
+}
